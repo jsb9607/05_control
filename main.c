536e15f1fce99c097a0e88c20a83fd568cbcf305
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-10-06 15:19:03 +0900

    05_1

--- main.c
+++ main.c
@@ -5,6 +5,24 @@
 
 int main(int argc, char *argv[]) {
 
+	int input;
+
+	printf("정수 하나를 입력하시오 : ");
+	scanf("%d", &input);
+
+	if(input > 0)
+	{
+		printf("양수입니다.\n");
+	}		
+	else if(input == 0)
+	{
+		printf("0 입니다.\n");
+	}
+	else
+	{
+		printf("음수입니다.\n");
+	}
+		
 	
 	return 0;
 }
